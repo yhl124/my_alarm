22e7230d624e123ff432a3652494b547ddb5125d
yhl124 <yhl124.py@gmail.com>
2023-12-03 22:29:17 +0900

    231203-2

--- lib/screens/set_page.dart
+++ lib/screens/set_page.dart
@@ -40,52 +40,51 @@ const List<(Days, String)> dayOfTheWeek = <(Days, String)>[
 ];
 
 class _SetHomePageState extends State<SetHomePage> {
-  final DateTime sixAM = //오늘 오전 6시
+
+  //오전 6시 이전 체크용, 0~5.99 true/ 6~23.99 false
+  bool _beforeSixAM = false;
+  //오늘 오전 6시
+  final DateTime sixAM = 
     DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 6, 0);
-  final DateTime now = DateTime.now();//오늘날짜&시간.
-  final DateTime today = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);//오늘
-  final DateTime tomorrow = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day).add(Duration(days: 1));//내일
+  //오늘 날짜
+  final DateTime today = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
 
-  //TimeofDay테스트
-  //TimeOfDay testtime = TimeOfDay(hour:1, minute:2);
-
-  //시간 기본값 초기화
-  DateTime time = DateTime.now();
-  //선택한 시간
-  DateTime _selectedTime = DateTime.now();
-  //오전 6시 이전 확인, 이전이면 true
-  bool beforeSixAM = false;
-  //달력 기본 선택날짜
-  List<DateTime?> _singleDatePickerValueWithDefaultValue = [];
-  //달력에서 선택한 날짜 기본값
-  DateTime _selectedDate = DateTime.now();
   //달력에서 선택을 했으면
   bool _selectedCal = false;
+  //달력에서 오늘을 선택 했으면
+  bool _selectedToday = false;
+  //달력 기본 선택날짜 리스트
+  List<DateTime?> _singleDatePickerValueWithDefaultValue = [];
 
+  //알람 선택 시간
+  TimeOfDay _selectedTime = TimeOfDay(hour: 6, minute: 0);
+  //알람 선택 날짜
+  DateTime _selectedDate = DateTime.now();
+
+  //선택한 요일이 하나라도 있으면 true
+  bool _selectedToggles = false;
   //요일 선택 리스트, 나중에는 Days의 개수나 dayOfTheWeek의 개수 가져와서 그 크기의 false리스트 만들면 될듯
   final List<bool> _toggleButtonsSelection = [false, false, false, false, false, false, false];
 
   TextEditingController _textController = TextEditingController();
 
+  //알람 울릴 날을 표시하는 텍스트를 위한 리스트
+  List<String> _DaysForDisplay = [];
+
   @override
   void initState(){
     super.initState();
 
-    //시계에서 선택한 시간과 오전 6시인지 체크, 이전이면 true
-    beforeSixAM = now.compareTo(sixAM)>0 ? false : true;
-    //기본값은 현재시간이 오전6시 이후면 내일 오전 6시, 아니면 오늘 오전6시
-    time = now.compareTo(sixAM)>0 ? sixAM.add(Duration(days:1)) : sixAM;
-    //달력 기본 선택날짜 = time
-    _selectedDate = time;
-    _singleDatePickerValueWithDefaultValue = [time,];
-  
+    //지금이 오전 6시이전인지 체크, 이전이면 true
+    _beforeSixAM = DateTime.now().isBefore(sixAM) ? true : false;
+    //오전6시 이후면 기본 선택날짜는 내일
+    _selectedDate = _beforeSixAM? today: today.add(Duration(days: 1));
+    _singleDatePickerValueWithDefaultValue = [_selectedDate,];
+    _DaysForDisplay.add(DateFormat('MM월 dd일 E요일').format(_selectedDate).toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    //선택한 날짜 텍스트
-    var selectedDateStr = DateFormat('MM월 dd일 E요일').format(_selectedDate).toString();
-
     return WillPopScope(//뒤로가기 핸들링
       onWillPop: () => _onWillPop(context),
       child: Scaffold(
@@ -104,20 +103,22 @@ class _SetHomePageState extends State<SetHomePage> {
               height: MediaQuery.of(context).size.height * 0.30,
               child: CupertinoDatePicker(
                 mode: CupertinoDatePickerMode.time,
-                initialDateTime: time,
+                initialDateTime: sixAM,
                 use24hFormat: false,
                 onDateTimeChanged: (DateTime newTime) {
                   setState(() { 
-                    if(_selectedCal == false || _selectedDate.compareTo(tomorrow)<=0){
-                      if(beforeSixAM){
-                        _selectedDate = newTime.compareTo(now)>0 ? newTime : newTime.add(Duration(days:1));
-                      }
-                      else if(!beforeSixAM){
-                        _selectedDate = newTime.subtract(Duration(days:1)).compareTo(now)>0 ? newTime.subtract(Duration(days:1)) : newTime;
-                      }
+                    //debugPrint(newTime.toString());
+                    if(_selectedCal == false && _selectedToggles == false){
+                       _selectedDate = newTime.isAfter(DateTime.now()) ? newTime : newTime.add(Duration(days:1));
+                       _DaysForDisplay.clear();
+                       _DaysForDisplay.add(DateFormat('MM월 dd일 E요일').format(_selectedDate).toString());
                     }
-                    _selectedTime = newTime;
-                    //debugPrint(_selectedDate.toString());
+                    else if(_selectedCal == true && _selectedToday == true && _selectedToggles == false){
+                      _selectedDate = newTime.isAfter(DateTime.now()) ? newTime : newTime.add(Duration(days:1));
+                      _DaysForDisplay.clear();
+                      _DaysForDisplay.add(DateFormat('MM월 dd일 E요일').format(_selectedDate).toString());
+                    }
+                    _selectedTime = TimeOfDay.fromDateTime(newTime);
                   });
                 },
               ),
@@ -128,8 +129,7 @@ class _SetHomePageState extends State<SetHomePage> {
               height: MediaQuery.of(context).size.height * 0.08,
               child: Row(
                 children: [
-                  //요일 한글화 필요
-                  Text(selectedDateStr, style: TextStyle(fontSize: 15),),//선택한 날짜 or 요일 표시 텍스트
+                  Text(_DaysForDisplay.toString(), style: TextStyle(fontSize: 15),),//선택한 날짜 or 요일 표시 텍스트
                   Expanded(child: Container(),),
                   CupertinoButton(//날짜 선택 버튼
                     padding: EdgeInsets.all(5),
@@ -146,8 +146,16 @@ class _SetHomePageState extends State<SetHomePage> {
                       ).then((selectedDate) {
                         setState(() {
                           if(selectedDate != null){//취소하면 null ok면 notnull
+                            //선택값이 리스트에 저장되는데 이 달력은 날짜 한개만 선택하기 때문에 리스트 첫번째값을 선택날짜로
                             _selectedDate = selectedDate[0] as DateTime;
+                            //달력 선택을 했으면
                             _selectedCal = true;
+                            //달력에서 오늘을 선택했으면 true 아니면 false
+                            _selectedToday = DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day).compareTo(today)==0
+                              ? true : false;
+                            //달력에서 선택하면 리스트를 비우고 그 날짜 추가
+                            _DaysForDisplay.clear();
+                            _DaysForDisplay.add(DateFormat('MM월 dd일 E요일').format(_selectedDate).toString());
                             //debugPrint(selectedDate.toString());
                           }
                         });
@@ -166,15 +174,37 @@ class _SetHomePageState extends State<SetHomePage> {
                   isSelected: _toggleButtonsSelection,
                   onPressed: (int index) {
                     setState(() {
-                    _toggleButtonsSelection[index] = !_toggleButtonsSelection[index];
+                      _toggleButtonsSelection[index] = !_toggleButtonsSelection[index];
+                      //debugPrint(_toggleButtonsSelection.toString());
+
+                      //선택된 요일의 인덱스를 리스트에 저장
+                      List<int> selectedIndexes = [];
+                      for(int i=0; i<_toggleButtonsSelection.length; i++){
+                        if(_toggleButtonsSelection[i]){
+                          selectedIndexes.add(i);
+                        }
+                      }
+                      //출력용 리스트에 요일 추가
+                      _DaysForDisplay.clear();
+                      _DaysForDisplay.add('매주 ');
+                      _selectedToggles = true;
+                      selectedIndexes.forEach((index) {
+                        _DaysForDisplay.add(dayOfTheWeek[index].$2);
+                      },);
+                      if(selectedIndexes.isEmpty){
+                        _selectedToggles = false;
+                        _DaysForDisplay.clear();
+                        _DaysForDisplay.add(DateFormat('MM월 dd일 E요일').format(_selectedDate).toString());
+                      }
                     });
                   },
                   children: 
-                    //아마도 리스트.map으로 <Days, String>으로 이루어진 리스트에서 2번째 값을 text로 가져와 리스트로 변환하는듯
                     dayOfTheWeek.map(((Days, String) day) => Text(day.$2)).toList(),
                 ),
               )
             ),
+            Container(height: 10.0, padding: EdgeInsets.fromLTRB(30, 0, 30, 0),),
+            SettingBlock(maintext: '공휴일에는 끄기', subtext: '설정된 값 표시', nextpage: EmptyPage()),
             //알람이름 설정
             Container(
               padding: EdgeInsets.fromLTRB(30.0, 27.0, 30.0, 13.0),
@@ -193,7 +223,7 @@ class _SetHomePageState extends State<SetHomePage> {
             SettingBlock(maintext: '다시 울림', subtext: '설정된 값 표시', nextpage: EmptyPage()),
           ],
         ),
-        //하단 취소 저장 //키보드에 바텀바가 안올라오게 설정 필요
+        //하단 취소 저장
         bottomNavigationBar: BottomAppBar(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
